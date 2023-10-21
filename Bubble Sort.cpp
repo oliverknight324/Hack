3cedb6fdfbcccbfc04920596f2b87aa62adaae4a
oliverknight324 <92440393+oliverknight324@users.noreply.github.com>
2023-10-21 15:46:10 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -29,9 +29,9 @@ void printArray(int arr[], int size)
 int main() 
 { 
 	int arr[] = { 5, 1, 4, 2, 8}; 
-	int M = sizeof(arr) / sizeof(arr[0]); 
-	bubbleSort(arr, M); 
+	int K = sizeof(arr) / sizeof(arr[0]); 
+	bubbleSort(arr, K); 
 	cout << "Sorted array: \n"; 
-	printArray(arr, M); 
+	printArray(arr, K); 
 	return 0; 
 }
